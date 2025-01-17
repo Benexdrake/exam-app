a2687876eb706e2e17529a518dfbb210d1938a32
BeneX Drake <benex@hotmail.de>
2025-01-17 09:46:42 +0100

    adding other root colors for testing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,13 +4,23 @@ import style from '@/styles/navbar.module.css'
 
 export default function Navbar()
 {
+
+    // const test = () =>
+    // {
+    //     document.documentElement.style.setProperty('--color1','#303841')
+    //     document.documentElement.style.setProperty('--color2','#3A4750')
+    //     document.documentElement.style.setProperty('--color3','#EA9215')
+    //     document.documentElement.style.setProperty('--color4','#EEEEEE')
+    // }
+
     return (
         <nav className={style.navbar_body}>
             <div className={style.navbar_links}>
                 <ul>
                 <Link href="/exam/1"> <li className={style.navbar_item}>AWS CCP EXAM</li> </Link>
                 <Link href="/practice/1"> <li className={style.navbar_item}>AWS CCP PRACTICE</li> </Link>
-                <Link href="/"> <li className={style.navbar_item}>HOME</li> </Link>
+                <Link href="/"> <li className={style.navbar_item}>HOME</li>
+                </Link>
                 <Link href="/exam/2"> <li className={style.navbar_item}>AWS DVA EXAM</li> </Link>
                 <Link href="/practice/2"> <li className={style.navbar_item}>AWS DVA PRACTICE</li> </Link>
                 </ul>
